02d44f4c50fa87d90a266c1d2dc792070e61c88d
Rego <pvrego@users.noreply.github.com>
2023-05-04 16:19:28 -0300

    Changes in tokenomics behavior.

--- contracts/governance-token.sol
+++ contracts/governance-token.sol
@@ -11,53 +11,83 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 private constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     uint256 public max_supply = 0;
-    uint256 public max_supply__RedefinitionDate = 0;
-    uint256 public max_supply__RedefinitionPeriod = 0;
+    uint256 public max_supply__LastChangeOn = 0;
+    uint256 public max_supply__AdjustmentSpan = 0;
+    bool inflationChangesAllowed = false;
+    
+    uint256 max_supply__InflationRatePct = 0;
+    
     uint256 public available_mint = 0;
-
+    
     constructor() ERC20("0xJournal", "0xJ") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(BURNER_ROLE, msg.sender);
         
         max_supply = 220000 * decimals();
-        max_supply__RedefinitionDate = block.timestamp;
-        max_supply__RedefinitionPeriod = 365 days;
+
+        max_supply__LastChangeOn = block.timestamp;
+        max_supply__AdjustmentSpan = 365 days;
+        max_supply__InflationRatePct = 2;
+
         available_mint = max_supply;
+
     }
+
+    // TEMPORARY!
+	function decimals() public pure override returns (uint8) {
+		return 0;
+	}
 
     /// Mint
 
-    uint256 mint_inflation_maxrate__pct = 2;
-
-    function setMint_Params(uint ratepct, uint redefinition_period) public onlyRole(DEFAULT_ADMIN_ROLE){
+    function runInflation() public onlyRole(DEFAULT_ADMIN_ROLE){
+        require(available_mint == 0, 'There is still available mints to be made before allowing to run inflation.');
+    
         uint timenow = block.timestamp;
-        uint duration = timenow - max_supply__RedefinitionPeriod;
-        require(duration >= max_supply__RedefinitionPeriod, 'Period spam for mint params redefinition has not been reached yet.');
+        uint duration = timenow - max_supply__LastChangeOn;
+        require(duration >= max_supply__AdjustmentSpan, 'Span for mint params redefinition has not been reached yet.');
         
-        mint_inflation_maxrate__pct = ratepct;
-        max_supply__RedefinitionPeriod = redefinition_period;
+        uint add_supply = max_supply * max_supply__InflationRatePct / 100;
+        max_supply += add_supply;
+        available_mint = add_supply;
+
+        max_supply__LastChangeOn = timenow;
+
+        inflationChangesAllowed = true;
+    }
+
+    function setInflationParams(uint ratePct, uint span) public onlyRole(DEFAULT_ADMIN_ROLE){
+        require(inflationChangesAllowed, 'Changes on inflation are only allowed after an inflation round.');
+        require(span >= 180 days, 'Inflation period is required to be greater than 180 days');
+
+        max_supply__InflationRatePct = ratePct;
+        max_supply__AdjustmentSpan = span;
+        inflationChangesAllowed = false;
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(to != address(0), 'Null address.');
+        require(amount > 0, 'Amount not positive.');
+        require(available_mint > 0, 'Not available mintable tokens.');
         require(amount <= available_mint, "Amount surpasses available mintable");
+        assert(available_mint < available_mint - amount);
+
         _mint(to, amount);
         available_mint -= amount;
-
-        if (available_mint == 0){
-            uint timenow = block.timestamp;
-            uint duration = timenow - max_supply__RedefinitionPeriod;
-            require(duration >= max_supply__RedefinitionPeriod, 'Period spam for max supply redefinition has not been reached yet.');
-
-            max_supply *= (100 + mint_inflation_maxrate__pct) / 100;
-            max_supply__RedefinitionDate = timenow;
-            available_mint = max_supply;
-        }
     }
 
-    function mint_available(address to) public onlyRole(MINTER_ROLE) {
-        uint amount = available_mint;
-        mint(to, amount);
+    function mint_allAvailable(address to) public onlyRole(MINTER_ROLE) {
+        mint(to, available_mint);
+    }
+
+    /// Burn
+
+    function burn(uint256 amount) public override onlyRole(BURNER_ROLE){
+        require(amount > 0, 'Burn number shall be non-zero positive');
+        require(balanceOf(msg.sender) >= amount, 'Not enough tokens to burn this amount.');
+
+        burn(amount);
     }
 
 }
