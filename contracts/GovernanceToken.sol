f5411b17b5273839ee1202b433d33230cf390cdb
Rego <pvrego@users.noreply.github.com>
2023-05-09 21:12:04 -0300

    Include natspec and remove mint_all_available() function.

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./AccessControl.sol";
 
 /// @title GovernanceToken
-/// @custom:version 0.4a
-/// @custom:security-contact contact@0xjournal.com
+/// @custom:version 0.5
+/// @custom:security-contact support@0xjournal.com
 contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
 
     uint256 public constant MAX_CAP = 500_000_000; /// Limit of max supply. In units of token (no decimals)
@@ -37,14 +37,29 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
     /* TODOs :
     - [ok] Remove OpenZepelin AccessControl : lots of warnings
     - [ok] Think about that makes the most sense regarding the SPAN limits : maybe it should be >60 days and always be <=365 days?
-    - Natspec : document all functions and variables
+    - [ok] Natspec : document all functions and variables
     - Publish the design, for each function write a mermaid diagram
     - Update the readme doc.
     - Verify code at testnet
     */
 
-    /// Mint
-
+    /**
+    * @notice Allows the admin to run inflation by adjusting the minting parameters.
+    * @param enable A boolean value indicating whether to run inflation or not.
+    *
+    * Requirements :
+    * - This function can only be called by the admin.
+    * - This function can only be called if there are no available mints left.
+    * - This function can only be called if the maximum supply has not yet reached the maximum cap.
+    * - This function can only be called if the span for mint parameter redefinition has been reached.
+    * - This function can only be called if inflation tuning is already active.
+    *
+    * Logics :
+    * - If `enable` is true, the function will calculate the additional supply based on the inflation rate, and adjust the available mints and maximum supply accordingly. If the sum of the maximum supply and additional supply is greater than or equal to the maximum cap, the available mints and maximum supply will be set to the maximum cap. Otherwise, the available mints will be set to the additional supply, and the maximum supply will be increased by the additional supply. The inflation tuning flag will be set to true.
+    * - If `enable` is false, the inflation tuning flag will be set to true (and no change will be made in current inflation params for the current period).
+    * - This function will emit an `InflationRun` event.
+    * - Throws an error if any of the above conditions are not met.
+    */
     function runInflation(bool enable) public requireAdmin {
         require(
             available_mint == 0,
@@ -59,7 +74,7 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
             "Span for mint params redefinition has not been reached yet."
         );
         
-        // Inflation changes are already active : this means that runInflation() has been already run once at this period.
+        // Inflation tuning is already active : this means that runInflation() has been already run once at this period.
         require(!inflation_tuning_active, 'Inflation changes are already active.'); 
         
         if (enable){
@@ -82,7 +97,22 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
         emit InflationRun(enable);
     }
 
-    function setInflationParams(uint256 newRatePct, uint256 newSpan)
+    /**
+    * @notice Set the inflation parameters for the next inflation round.
+    * @param newRatePct The new inflation rate as a percentage. Must be less than or equal to MAX_RATE.
+    * @param newSpan The new span for mint parameter redefinition, in seconds. Must be between SPAN_MIN and SPAN_MAX.
+    *
+    * Requirements:
+    * - The function must be called by an admin.
+    * - The inflation parameters must be tunable (i.e. an inflation round must have occurred).
+    * - The new span must be between SPAN_MIN and SPAN_MAX.
+    * - The new inflation rate must be less than or equal to MAX_RATE.
+    *
+    * Logics :
+    * - Updates the inflation parameters
+    * - Emits an {InflationTuned} event with the new inflation rate and span.
+    */
+    function tuneInflation(uint256 newRatePct, uint256 newSpan)
         public
         requireAdmin
     {
@@ -105,6 +135,23 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
         inflation_tuning_active = false;
     }
 
+    /**
+    * @notice Mints new tokens and assigns them to the specified address.
+    * @param to The address that will receive the minted tokens.
+    * @param amount The amount of tokens to mint, in units of token (no decimals).
+    *
+    * Requirements:
+    * - The caller must be a minter of the contract.
+    * - The address `to` must not be the null address.
+    * - The `amount` must be greater than 0.
+    * - There must be available mintable tokens to mint.
+    * - The `amount` being minted must not exceed the available mintable tokens.
+    *
+    * Effects:
+    * - Increases the total supply of tokens by `amount` multiplied by 10^decimals.
+    * - Decreases the available mintable tokens by `amount`.
+    * - Emits a {Mint} event indicating the address that received the minted tokens and the amount minted.
+    */
     function mint(address to, uint256 amount /*in units of token (no decimals*/ ) public requireMinter {
         require(to != address(0), "Null address.");
         require(amount > 0, "Amount not positive.");
@@ -121,12 +168,19 @@ contract GovernanceToken is ERC20, ERC20Burnable, AccessControl {
         emit Mint(to, amount);
     }
 
-    function mint_allAvailable(address to) public requireMinter {
-        mint(to, available_mint);
-    }
-
-    /// Burn
-
+    /**
+    * @notice Burns a specific amount of tokens from the caller's account, reducing the total supply.
+    * @param amount uint256 The amount of tokens to be burned, specified in units of token (no decimals).
+    * 
+    * Requirements:
+    * - amount shall be non-zero positive.
+    * - The caller must have enough tokens to burn this amount.
+    * 
+    * Logics : 
+    * - amount of tokens will be removed from the caller's account.
+    * - max_supply will be reduced by amount.
+    * Emits an {Burn} event indicating the amount burned and the burner address.
+    */
     function burn(uint256 amount) public override requireBurner {
         require(amount > 0, "Burn number shall be non-zero positive.");
         require(
