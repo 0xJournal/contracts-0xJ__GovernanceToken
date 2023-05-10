85752d49e52270be19b00b2814dd8dbcc8057fbc
Rego <pvrego@users.noreply.github.com>
2023-05-10 09:26:50 -0300

    Included localfile for OpenZeppelin dependencies.

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -33,8 +33,9 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./OpenZeppelin-Deps_flattened.sol";
 import "./AccessControl.sol";
 
 /**
